002973404c20b151c3f037554fa385f374ecf31c
AndyTechPro <chukwuandrew2025@gmail.com>
2023-12-02 23:31:04 +0100

    fix 10

--- src/Pages/SearchResults.js
+++ src/Pages/SearchResults.js
@@ -60,7 +60,7 @@ const SearchResults = () => {
             <div key={result._id} className="post2">
               <div className="image2">
                 <Link to={`/post/${result._id}`}>
-                  <img src={`/uploads/${result.cover}`} alt="img not showing" />
+                  <img  src={`https://ninejaback.onrender.com/${result.cover}`} alt="img not showing" />
                 </Link>
               </div>
               <div className="texts2">
